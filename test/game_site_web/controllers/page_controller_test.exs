aa412655450cdd52f756892cedce56a969109952
emilssudmalis1337 <emils.sudmalis@gmail.com>
2026-01-09 20:40:51 +0200

    Delete test directory

--- test/game_site_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GameSiteWeb.PageControllerTest do
-  use GameSiteWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
